0695e2d38e3d2dcdefe067dfcc9930653cef2869
mfahmialif <mfahmialif@gmail.com>
2025-10-10 21:46:20 +0700

    nullable update

--- simkeu.app/database/migrations/2025_07_27_080408_create_keuangan_deposit.php
+++ simkeu.app/database/migrations/2025_07_27_080408_create_keuangan_deposit.php
@@ -11,7 +11,7 @@ return new class extends Migration
             $table->id();
             $table->string('nim');
             $table->double('jumlah');
-            $table->text('keterangan')->nullable;
+            $table->text('keterangan')->nullable();
             $table->timestamps();
         });
     }
