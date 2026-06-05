18506db0479d970105a44c2e126f5192b9fa8c63
mfahmialif <mfahmialif@gmail.com>
2026-06-05 22:52:52 +0700

    update backend sync sp

--- simkeu.app/routes/web.php
+++ simkeu.app/routes/web.php
@@ -8,10 +8,10 @@ Route::get('/', function () {
 });
 
 Route::get('testing', [TestingController::class, 'index']);
-Route::get('tes-pmb', [TestingController::class, 'tesPembayaranPmb']);
-Route::get('tes-input-wisuda', [TestingController::class, 'tesInputWisuda']);
-Route::get('list-pembayaran-sp', [TestingController::class, 'listPembayaranSP']);
-Route::get('sp-belum-terinput', [TestingController::class, 'spBelumTerinput']);
-Route::get('tagihan-sp-belum-dibayar', [TestingController::class, 'listTagihanSpBelumDibayar']);
-Route::get('input-sp-belum-terinput', [TestingController::class, 'inputSpBelumTerinput']);
-Route::get('rollback-input-sp-belum-terinput', [TestingController::class, 'rollbackInputSpBelumTerinput']);
+// Route::get('tes-pmb', [TestingController::class, 'tesPembayaranPmb']);
+// Route::get('tes-input-wisuda', [TestingController::class, 'tesInputWisuda']);
+// Route::get('list-pembayaran-sp', [TestingController::class, 'listPembayaranSP']);
+// Route::get('sp-belum-terinput', [TestingController::class, 'spBelumTerinput']);
+// Route::get('tagihan-sp-belum-dibayar', [TestingController::class, 'listTagihanSpBelumDibayar']);
+// Route::get('input-sp-belum-terinput', [TestingController::class, 'inputSpBelumTerinput']);
+// Route::get('rollback-input-sp-belum-terinput', [TestingController::class, 'rollbackInputSpBelumTerinput']);
